bcf34e9a0f6cfeb808dc78b502f9066a643d4e51
Daniils_Kackins <daniil.kackin@gmail.com>
2023-07-01 10:31:50 +0100

    Добавил решение допа

---
+++ Task_004/Program.cs
@@ -0,0 +1,16 @@
+﻿// Дана строка, найти в ней количество заглавных и строчных букв. Для нахождения количеств написать два собственных метод
+
+Console.WriteLine("Введите строку: ");
+int Uppercase = 0;
+int Lowercase = 0;
+int Digit = 0;
+int Others = 0;
+string? line = Console.ReadLine();
+foreach (var symbol in line)
+{
+    if (char.IsUpper(symbol)) Uppercase++;
+    else if (char.IsLower(symbol)) Lowercase++;
+    else if (char.IsDigit(symbol)) Digit++;
+    else if (char.IsSymbol(symbol)) Others++;
+}
+Console.WriteLine($"В указанной строке:\nЗаглавных - {Uppercase}\nСтрочных - {Lowercase}\nЦифр - {Digit}\nСимволов - {Others}");
